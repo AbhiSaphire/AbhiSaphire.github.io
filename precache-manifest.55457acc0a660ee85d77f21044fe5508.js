d339ddba885b326a1f78fb7cfca132306f1c29c6
Abhishek <AbhiSaphire@github.com>
2020-04-26 01:34:13 +0530

    Updates

---
+++ precache-manifest.55457acc0a660ee85d77f21044fe5508.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca69d94eb389b8cc491d4fae151f4c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b1c5ff8ff3378eafbe4",
+    "url": "/static/css/main.a3f5a7a9.chunk.css"
+  },
+  {
+    "revision": "35e39b1ff1207d686b95",
+    "url": "/static/js/2.451a2ad7.chunk.js"
+  },
+  {
+    "revision": "8b1c5ff8ff3378eafbe4",
+    "url": "/static/js/main.9862a34d.chunk.js"
+  },
+  {
+    "revision": "efcce9e3a90d19e09864",
+    "url": "/static/js/runtime-main.bb72e66d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a754ce12bab2baa231970fbdf0bda08d",
+    "url": "/static/media/abhi.a754ce12.png"
+  },
+  {
+    "revision": "8da6e1c166716511c6dc985b5a5d0d81",
+    "url": "/static/media/abhi1.8da6e1c1.png"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "588934123ecb35eb0278bc29d8a7e5e5",
+    "url": "/static/media/cc_ghrcem.58893412.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "78f13d85d4dcb9dd69b9735208d9fe70",
+    "url": "/static/media/feelingProud.78f13d85.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "b146fee899df65f12d57d3b0e485c5aa",
+    "url": "/static/media/ghrcem_logo.b146fee8.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "28b1b03d881f41d22968e0e1fde3a09c",
+    "url": "/static/media/internshala_logo.28b1b03d.png"
+  },
+  {
+    "revision": "858096f5b9e0f8191cdc76c373e49f95",
+    "url": "/static/media/logo.858096f5.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "7db8e0d135747a86f4c122365aefaa6f",
+    "url": "/static/media/relax.7db8e0d1.svg"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "905b9766ddee9714d24aa260ed99b26d",
+    "url": "/static/media/udemy.905b9766.svg"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
